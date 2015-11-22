5fa4a2e5e3befefa5fa44f63c39c925f1a67be2c
SherryShih <sherryshih@sherryshihtekimacbook-air.local>
2015-11-22 18:42:15 +0800

    Initial Commit

---
+++ Tour01Helloworld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tour01Helloworld
+//
+//  Created by SherryShih on 2015/11/22.
+//  Copyright © 2015年 sherry. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
